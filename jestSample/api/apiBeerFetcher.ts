d7681a1c314e1ff13246d21ead1e4274d0af5c9b
YamazakiYusuke <yusuke38098@gmail.com>
2023-06-22 07:37:38 +0900

    課題４
    - 問題作成
    
    (cherry picked from commit 22e097ef1f511fceb1c148e876b5c3dbd8ebac7f)

---
+++ jestSample/api/apiBeerFetcher.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosError } from "axios";
+
+export interface IApiBeerFetcher {
+    fetch(): Promise<string>;
+}
+
+/**
+ * `ApiBeerFetcher`クラスはビールデータを非同期に取得します。
+ * `IApiBeerFetcher` インターフェースを実装しています。
+ *
+ * @remarks
+ * このクラスは`axios`を用いて"https://random-data-api.com/api/v2/beers"からデータをフェッチします。
+ * リクエストのタイムアウトは1から2000ミリ秒のランダムな値です。
+ *
+ * @example
+ * ```ts
+ * const fetcher = new ApiBeerFetcher();
+ * const beerData = await fetcher.fetch();
+ * console.log(beerData);
+ * ```
+ *
+ * @implements {IApiBeerFetcher}
+ * @public
+ */
+export class ApiBeerFetcher implements IApiBeerFetcher {
+
+    /**
+     * ランダムなビールデータを非同期に取得します。
+     * レスポンス中のビール名を文字列として返します。
+     * リクエストがタイムアウトした場合やデータ取得に失敗した場合は、適切なエラーメッセージを返します。
+     * 
+     * @returns {Promise<string>} フェッチしたビールデータの名前またはエラーメッセージ。
+     * @throws {AxiosError} リクエストのエラー情報。
+     * @async
+     * @public
+     */
+    public async fetch(): Promise<string> {
+        try {
+            const randomInt = Math.floor(Math.random() * 2000) + 1;
+            const { data } = await axios.get(
+                "https://random-data-api.com/api/v2/beers", { timeout: randomInt }
+            );
+            console.log(data);
+            return data.name as string;
+        } catch (error) {
+            console.error(error);
+            if ((error as AxiosError).message.includes('timeout')) {
+                return 'ApiBeerFetcher request timed out';
+            } else {
+                return 'ApiBeerFetcher failed to fetch data';
+            }
+        }
+    }
+}
