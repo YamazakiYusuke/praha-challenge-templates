9ff0d4fcc8d82f1789906b2f826fe6dac187d337
YamazakiYusuke <yusuke38098@gmail.com>
2023-06-24 09:28:52 +0900

    indent spaceを2に統一

--- jestSample/api/apiNameFetcher.ts
+++ jestSample/api/apiNameFetcher.ts
@@ -1,29 +1,29 @@
 import axios from "axios";
 
 export interface IApiNameFetcher {
-    fetch(): Promise<string>;
+  fetch(): Promise<string>;
 }
 
 export class ApiNameFetcher implements IApiNameFetcher {
-    public async fetch(): Promise<string> {
-        const { data } = await axios.get(
-            "https://random-data-api.com/api/name/random_name"
-        );
-        return data.first_name as string;
-    }
+  public async fetch(): Promise<string> {
+    const { data } = await axios.get(
+      "https://random-data-api.com/api/name/random_name"
+    );
+    return data.first_name as string;
+  }
 }
 /**
  * ApiNameFetcherのMock
  * インスタンス生成時に任意のfirstNameをセット
  */
 export class ApiNameFetcherMock implements IApiNameFetcher {
-    private firstName: string;
+  private firstName: string;
 
-    public constructor(firstName: string) {
-        this.firstName = firstName;
-    }
+  public constructor(firstName: string) {
+    this.firstName = firstName;
+  }
 
-    public async fetch(): Promise<string> {
-        return this.firstName;
-    }
+  public async fetch(): Promise<string> {
+    return this.firstName;
+  }
 }
